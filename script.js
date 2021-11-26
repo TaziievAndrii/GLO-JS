4004e6483813681190a80ab2a90ed1ec71c06da5
Taziiev Andrii <andrew13369@icloud.com>
2021-11-26 11:26:52 +0200

    раделил переменные и console

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
 let title = " calculator ";
-console.log("[title]", typeof title);
-
-let screens = " Простые, Сложные, Интерактивные ";
-console.log("[screens]", screens.length);
-
+let screens = "Простые, Сложные, Интерактивные";
 let arrayScreens = screens.toLowerCase().split(", ");
-console.log("[arrayScreens]", arrayScreens);
-
 let screenPrice = 300;
-console.log("[screenPrice]", screenPrice + " долларов");
-
 let rollback = 27;
-
 let fullPrice = 1500;
+let rollbackPercentage = fullPrice * (rollback / 100);
+let adaptive = true;
+
+
+console.log("[title]", typeof title);
+console.log("[screens]", screens.length);
+console.log("[arrayScreens]", arrayScreens);
+console.log("[screenPrice]", screenPrice + " долларов");
 console.log("[fullPrice]", typeof fullPrice);
 console.log("[fullPrice]", fullPrice + " долларов");
-
-let rollbackPercentage = fullPrice * (rollback / 100);
 console.log("[rollbackPercentage]", rollbackPercentage);
-
-let adaptive = true;
 console.log("[adaptive]", typeof adaptive);
