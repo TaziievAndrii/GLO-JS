bb7168d69ce190bddb6b2ab5da6efd4ce326223f
Taziiev Andrii <andrew13369@icloud.com>
2021-11-26 08:10:12 +0200

    задание второго урока

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = " calculator ";
+console.log("[title]", typeof title);
 
-alert('Hello GLO');
-console.log('START');
+let screens = " Простые, Сложные, Интерактивные ";
+console.log("[screens]", screens.length);
+
+let arrayScreens = screens.toLowerCase().split(", ");
+console.log("[arrayScreens]", arrayScreens);
+
+let screenPrice = 300;
+console.log("[screenPrice]", screenPrice + " долларов");
+
+let rollback = 27;
+
+let fullPrice = 1500;
+console.log("[fullPrice]", typeof fullPrice);
+console.log("[fullPrice]", fullPrice + " долларов");
+
+let rollbackPercentage = fullPrice * (rollback / 100);
+console.log("[rollbackPercentage]", rollbackPercentage);
+
+let adaptive = true;
+console.log("[adaptive]", typeof adaptive);
