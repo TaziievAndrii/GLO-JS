13af5ccff0bb12ae9cf4b3b5f9c4d1aa767ca005
Taziiev Andrii <andrew13369@icloud.com>
2021-11-29 12:32:44 +0200

    добавил комиты, заменил let на const

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
 'use strict'
 
+// Узнаем название проекта и сохраняем в переменную
 let title = prompt("Как называется ваш проект ?");
+
+// Узнаем типы о типах экранов для разработки
 let screens = prompt("Какие типы экранов нужно разработать?", "пример: Простые, Сложные, Интерактивные");
-let arrayScreens = screens.toLowerCase().split(", ");
+
+// Данные об экраннах переводим в нижний регистр
+const arrayScreens = screens.toLowerCase().split(", ");
+
+// Узнаем стоимость работы
 let screenPrice = +prompt("Сколько будет стоить данная работа?", "пример: 12000");
-let rollback = 27;
+
+// Задаем ставку отката
+const rollback = 27;
+
+// Узнаем про адаптацию экрана 
 let adaptive = confirm("Нужен ли адаптив на сайте?");
 
+// Узнаем про дополнительные услуги и их стоимость 
 let service1 = prompt("Какой дополнительный тип услуги нужен?", "например: service1");
 let servicePrice1 = +prompt("Сколько это будет стоить?", "например: 1000");
 
 let service2 = prompt("Какой дополнительный тип услуги нужен?", "например: service2");
 let servicePrice2 = +prompt("Сколько это будет стоить?", "например: 1000");
+//! Узнаем про дополнительные услуги и их стоимость 
 
+// Рассчитываем стоимость всех услуг
 let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-let rollbackPercentage = fullPrice * (rollback / 100);
-let servicePercentPrice = Math.ceil(fullPrice - rollbackPercentage);
 
+// Узнаем сумму отката посреднику
+let rollbackPercentage = fullPrice * (rollback / 100);
+
+// Просчитываем чистый доход за проект
+const servicePercentPrice = Math.ceil(fullPrice - rollbackPercentage);
+
+// Условия предоставления скидки
 switch (true) {
     case fullPrice >= 30000:
         console.log("Даем скидку в 10%");
@@ -31,7 +50,6 @@ switch (true) {
         console.log("Что то пошло не так");
         break;
 }
-
 
 console.log("[title]", typeof title);
 console.log("[screens]", screens.length);
